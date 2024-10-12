adb1fc9574bd9ca10284169a398a1d6371781325
EmilioIzquierdo30 <emiliioizq23@gmail.com>
2024-10-11 20:30:51 -0600

    cree un slider para mi add y separe en componentes lo que voy a retulizar en otras paginas

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import React from 'react';
 import './App.css';
-import Navbar from './navbar'; 
+import Navbar from './componentes/navbar.js'; 
 import Main from './Main'; 
+import Slider from './componentes/slider.js';
 
 function App() {
+  
+  const mockImagenes = [
+    {
+      url: "https://i0.wp.com/revistamariaorsini.com/wp-content/uploads/2018/10/epazoteft.jpg?fit=948%2C622&ssl=1",
+      nombre: "Epazote",
+      descripcion: "El epazote es una planta utilizada en la gastronomía mexicana, conocida por sus propiedades medicinales y su sabor distintivo."
+    },
+    {
+      url: "https://imag.bonviveur.com/algunas-hojas-de-hierbabuena.jpg",
+      nombre: "Hierbabuena",
+      descripcion: "La hierbabuena es una hierba aromática utilizada en muchas culturas para dar sabor a platillos y bebidas, así como por sus propiedades digestivas."
+    },
+    {
+      url: "https://imag.bonviveur.com/corte-transversal-de-una-papaya.webp",
+      nombre: "Papaya",
+      descripcion: "La papaya es una fruta tropical rica en vitaminas y enzimas que ayudan a la digestión."
+    }
+  ]
+  
   return (
     <div className="container">
       <Navbar />
       <Main />
+      <Slider imagenes={mockImagenes}/>
     </div>
   );
 }
+
+
 
 export default App;
-
-function App() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Main />
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Main />
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Main />
-    </div>
-  );
-}
