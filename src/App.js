85d62a7ee768b0d517e9d9a755d1a40ad71f5096
EmilioIzquierdo30 <emiliioizq23@gmail.com>
2024-10-13 22:20:43 -0600

    Borrado algunos archivos tratando de arreglar un bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import './app.css';
 import Navbar from './componentes/navbar.js'; 
 import Main from './paginas/Main.js'; 
-import Slider from './componentes/slider.js';
 import AboutUs from './componentes/aboutus.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './paginas/login.js'; // Este es el import que enlaza el archivo
+import Catalogo from './paginas/catalogo.js';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/plants" element={<Slider />} />
+        <Route path="/plants" element={<Catalogo />} />
         <Route path="/about" element={<AboutUs />} />
       </Routes>
     </Router>
