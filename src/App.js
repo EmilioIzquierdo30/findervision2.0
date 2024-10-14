6d9872db60ce226d7f5360156a1ca4b3472939fb
EmilioIzquierdo30 <emiliioizq23@gmail.com>
2024-10-13 22:44:54 -0600

    arreglado el error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './app.css';
+import React from 'react'
+import './App.css'
 import Navbar from './componentes/navbar.js'; 
 import Main from './paginas/Main.js'; 
 import AboutUs from './componentes/aboutus.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './paginas/login.js'; // Este es el import que enlaza el archivo
-import Catalogo from './paginas/catalogo.js';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/plants" element={<Catalogo />} />
+        <Route path="/plants" element={<Login />} /> 
         <Route path="/about" element={<AboutUs />} />
       </Routes>
     </Router>
--- src/paginas/Main.js
+++ src/paginas/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '/app.css'; 
+import '../App.css';
 import Slider from '../componentes/slider';
 
 function Main() {
