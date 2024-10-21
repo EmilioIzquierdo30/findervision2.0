0f7f102c0149c6b4d3fe4da26c0066f00517722a
BrianSG819 <briansiasgarcia9@gmail.com>
2024-10-21 00:36:23 -0600

    mejores funciones

--- src/Planta.jsx
+++ src/Planta.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './Planta.css'; // Importa los estilos específicos para el componente Planta
 
 const Planta = ({ nombre, categoria, imagen, seleccionarPlanta }) => {
+  const defaultImage = "URL_de_imagen_por_defecto"; // Imagen por defecto si no hay imagen
+
   return (
     <div className="planta" onClick={seleccionarPlanta}>
-      <img src={imagen} alt={nombre} className="planta-imagen" />
+      <img src={imagen || defaultImage} alt={nombre} className="planta-imagen" />
       <div className="planta-info">
         <div className="planta-nombre">{nombre}</div>
         <div className="planta-categoria">{categoria}</div>
